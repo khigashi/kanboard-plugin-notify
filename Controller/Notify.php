5ca906447935a526be7ad33d429b695da0b570b8
Márcio Dias <khigashi.oang@gmail.com>
2017-01-15 17:31:52 -0200

    - Support for “task.update” notification - Notifications limited by 3 at a time - Fix notification Title, Description

--- Controller/Notify.php
+++ Controller/Notify.php
@@ -22,11 +22,39 @@ class Notify extends BaseController
 
 			foreach($obter_notificacoes AS $notificationInfo){
 				
+				
+				if($notificationInfo['event_name'] == "task.update"){
+					
+					$notify_description = $notificationInfo['event_data']['task']['title'];
+					
+					$notify_title = "[".$notificationInfo['event_data']['task']['project_name']."] ".$notificationInfo['title'];
+					
+				
+				}else{
+					
+					
+					if(is_array($notificationInfo['event_data']['tasks']) AND count($notificationInfo['event_data']['tasks']) > 0){
+						
+						$tasks_name = array();
+						foreach($notificationInfo['event_data']['tasks'] AS $sub_tasks){
+							$tasks_name[] = $sub_tasks['title'];
+						}
+						
+						$notify_description = implode(', ', $tasks_name);
+						
+					}else{
+						
+						$notify_description = $notificationInfo['event_data']['tasks']['title'];
+						
+					}
+					
+					$notify_title = "[".$notificationInfo['event_data']['project_name']."] ".$notificationInfo['title'];
+					
+				}
+				
+
 				//Get notification URL
 				$url_tarefa = $this->helper->url->to("WebNotificationController", "redirect", array("notification_id" => $notificationInfo['id'], 'user_id' => $user['id']), false, false, true);
-				
-				//Insert project name into Notification Title
-				$notify_titulo = "[".$notificationInfo['event_data']['task']['project_name']."] ".$notificationInfo['event_data']['task']['title'];
 				
 				//Insert logo from "Asset/img" folder
 				$notify_image = $this->configModel->get('application_url')."plugins/Notify/Asset/img/logo.png";
@@ -36,10 +64,15 @@ class Notify extends BaseController
 				
 			//Check notification in SESSION
 			if(!@$this->container['sessionStorage']->$id_notificacao){
-
+					
+					//Avoid show this notification again
+					
+					if($contador_notificacoes <= 3){
+						
+						
 					$arrayNotificacoes['notificacoes'][] = array(
-					"titulo" => $notify_titulo,
-					"descricao" => $notificationInfo['title'],
+					"titulo" => $notify_title,
+					"descricao" => $notify_description,
 					"url" => $url_tarefa,
 					"data" => $notificationInfo['date_creation'],
 					"imagem" => $notify_image,
@@ -47,9 +80,10 @@ class Notify extends BaseController
 					);
 						
 					$contador_notificacoes++;
-					
-					//Avoid show this notification again
-					$this->container['sessionStorage']->$id_notificacao = '1';
+						
+						
+						$this->container['sessionStorage']->$id_notificacao = '1';
+					}
 				
 				}
 			}
